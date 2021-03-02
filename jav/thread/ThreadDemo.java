a120ad942573e22321a1fdf5d31f05fcd03f1643
Md. Rakib Hasan <RakibHasanRiyad@users.noreply.github.com>
2021-03-03 03:07:32 +0600

    Add files via upload
    Java Multi Threading

---
+++ jav/thread/ThreadDemo.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jav.thread;
+
+/**
+ *
+ * @author HP
+ */
+class ThreadTDemo extends Thread {
+   private Thread t;
+   private String threadName;
+   
+   ThreadTDemo( String name) {
+      threadName = name;
+      System.out.println("Creating " +  threadName );
+   }
+   
+   public void run() {
+      System.out.println("Running " +  threadName );
+      try {
+         for(int i = 4; i > 0; i--) {
+            System.out.println("Thread: " + threadName + ", " + i);
+            // Let the thread sleep for a while.
+            Thread.sleep(50);
+         }
+      } catch (InterruptedException e) {
+         System.out.println("Thread " +  threadName + " interrupted.");
+      }
+      System.out.println("Thread " +  threadName + " exiting.");
+   }
+   
+   public void start () {
+      System.out.println("Starting " +  threadName );
+      if (t == null) {
+         t = new Thread (this, threadName);
+         t.start ();
+      }
+   }
+}
+
+public class ThreadDemo {
+
+   public static void main(String args[]) {
+      ThreadTDemo T1 = new ThreadTDemo( "Thread-1");
+      T1.start();
+      
+      ThreadTDemo T2 = new ThreadTDemo( "Thread-2");
+      T2.start();
+   }   
+}
---
+++ jav/thread/ThreadSchedule.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jav.thread;
+
+/**
+ *
+ * @author HP
+ */
+class A extends Thread {
+
+    int i = 0;
+
+    public void run() {
+        System.out.println("Thread A started");
+        while (i < 4) {
+            System.out.println("\t value of i in Thread A:" + i);
+            i++;
+        }
+        System.out.println("ThreadA finished");
+    }
+}
+
+class B extends Thread {
+
+    public void run() {
+        int i = 0;
+        System.out.println("ThreadB started");
+        while (i < 4) {
+            System.out.println("\t value of i in Thread B:" + i);
+            i++;
+        }
+        System.out.println("ThreadB finished");
+    }
+}
+
+class C extends Thread {
+
+    public void run() {
+        int i = 0;
+        System.out.println("ThreadC started");
+        while (i < 4) {
+            System.out.println("\t value of i in Thread C" + i);
+            i++;
+        }
+        System.out.println("ThreadC finished");
+    }
+}
+
+public class ThreadSchedule {
+
+    public static void main(String[] args) {
+        System.out.println("Main Thread started");
+        A a = new A();
+        B b = new B();
+        C c = new C();
+        Thread th = Thread.currentThread();
+        System.out.println(th.getName());
+        th.setPriority(Thread.MAX_PRIORITY);
+        b.setPriority(Thread.MIN_PRIORITY);
+        c.setPriority(Thread.NORM_PRIORITY);
+        a.start();
+        b.start();
+        c.start();
+    }
+}
